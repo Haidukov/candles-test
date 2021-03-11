5ac7789f4aecae6c8cf0a5b987c985b8ea41c570
rhaidukov <rhaidukov@amcbridge.com>
2021-03-11 08:51:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/test', (req, res) => res.send('hello'));
+
+app.listen(8080, () => {
+    console.log('Application is listening port 8080');
+});
