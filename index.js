bed41053c53effbd3d98637b355aa6869b434b14
rhaidukov <rhaidukov@amcbridge.com>
2021-03-11 22:41:12 +0200

    send teltonika code

--- index.js
+++ index.js
@@ -19,6 +19,20 @@ const server = net.createServer((socket) => {
     });
 });
 
+const server2 = net.createServer(socket => {
+    let i = 0;
+    socket.on('data', data => {
+        if (i === 0) {
+            console.log(eval(data));
+            socket.write(printToDevice('01'));
+        }
+        const command = '2C 01 00 00 00 14';
+        const teltonikaCommand = `00 00 00 00 00 00 00 0E 0C 01 05 00 00 00 06 ${command} 01 00 00 00 00`;
+        socket.write(printToDevice(teltonikaCommand));
+        i++;
+    });
+});
+
 console.log(printToDevice('00 00 00 01'));
 
 /*
@@ -27,6 +41,9 @@ console.log(Buffer.from(data2));
 */
 
 
+server2.listen(5280, () => {
+    console.log('TCP is listening port 5280');
+});
 
 server.listen(5027, () => {
     console.log('TCP is listening port 5027');
